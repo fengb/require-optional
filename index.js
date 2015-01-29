70c983bd1d13334041fab59aa859392fcbbc8e33
fengb <contact@fengb.info>
2015-01-28 22:17:04 -0600

    Support flat syntax

--- index.js
+++ index.js
@@ -15,10 +15,21 @@ function requireOptional(require, name, def){
   return requireExists(require, name) ? require(name) : def
 }
 
-module.exports = function(require){
-  return function(name, def){
-    return requireOptional(require, name, def)
-  }
+function isRequire(val){
+  return !!val.resolve
 }
 
-module.exports.raw = requireOptional
+module.exports = function(arg1, arg2, arg3){
+  if(!isRequire(arg1)){
+    // use local require since it's the best we have
+    return requireOptional(require, arg1, arg2)
+  }
+
+  if(arg2 != undefined){
+    return requireOptional(arg1, arg2, arg3)
+  }
+
+  return function(name, def){
+    return requireOptional(arg1, name, def)
+  }
+}
--- test/index.js
+++ test/index.js
@@ -1,19 +1,53 @@
 var expect = require('chai').expect
-var requireOptional = require('..')(require)
+var requireOptional = require('..')
+
+var def = function(){}
 
 describe('requireOptional()', function(){
-  it('requires an existing module', function(){
-    var fromRequire = require('mocha')
-    expect(requireOptional('mocha')).to.equal(fromRequire)
+  describe('simple syntax', function(){
+    it('requires an existing module', function(){
+      var fromRequire = require('mocha')
+      expect(requireOptional('mocha')).to.equal(fromRequire)
+    })
+
+    it('returns default value if module does not exist', function(){
+      expect(requireOptional('missing', def)).to.equal(def)
+    })
   })
 
-  it('returns default value if module does not exist', function(){
-    var def = function(){}
-    expect(requireOptional('missing', def)).to.equal(def)
+  describe('fugly syntax', function(){
+    it('requires an existing module', function(){
+      var fromRequire = require('mocha')
+      expect(requireOptional(require, 'mocha')).to.equal(fromRequire)
+    })
+
+    it('requires relative files', function(){
+      var fromRequire = require('../package.json')
+      expect(requireOptional(require, '../package.json')).to.equal(fromRequire)
+    })
+
+    it('returns default value if module does not exist', function(){
+      expect(requireOptional(require, 'missing', def)).to.equal(def)
+    })
   })
 
-  it('requires relative files', function(){
-    var fromRequire = require('../package.json')
-    expect(requireOptional('../package.json')).to.equal(fromRequire)
+  describe('curried syntax', function(){
+    beforeEach(function(){
+      this.curriedRequireOptional = requireOptional(require)
+    })
+
+    it('requires an existing module', function(){
+      var fromRequire = require('mocha')
+      expect(this.curriedRequireOptional('mocha')).to.equal(fromRequire)
+    })
+
+    it('requires relative files', function(){
+      var fromRequire = require('../package.json')
+      expect(this.curriedRequireOptional('../package.json')).to.equal(fromRequire)
+    })
+
+    it('returns default value if module does not exist', function(){
+      expect(this.curriedRequireOptional('missing', def)).to.equal(def)
+    })
   })
 })
