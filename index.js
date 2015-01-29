c932a365297d3d3dd20ac5059a7d3651a1bfe886
fengb <contact@fengb.info>
2015-01-28 19:54:17 -0600

    First implementation

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = function(name, def){
+  try {
+    return require(name)
+  } catch (e) {
+    if(e.code === 'MODULE_NOT_FOUND'){
+      return def
+    } else {
+      throw e
+    }
+  }
+}
