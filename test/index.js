5204634e9ca30a858a4b66c9918978511ede8801
fengb <contact@fengb.info>
2015-11-09 18:54:55 -0600

    Linting issues

--- test/index.js
+++ test/index.js
@@ -5,26 +5,26 @@ var expect = chai.expect
 var sinon = require('sinon')
 var requireOptional = require('..')
 
-var def = function(){}
+var def = {}
 
-describe('requireOptional()', function(){
-  describe('simple syntax', function(){
-    it('requires an existing module', function(){
+describe('requireOptional()', function () {
+  describe('simple syntax', function () {
+    it('requires an existing module', function () {
       var fromRequire = require('mocha')
       expect(requireOptional('mocha')).to.equal(fromRequire)
     })
 
-    it('requires relative modules', function(){
+    it('requires relative modules', function () {
       var fromRequire = require('../package.json')
       expect(requireOptional('../package.json')).to.equal(fromRequire)
     })
 
-    context('missing', function(){
-      it('returns default value', function(){
+    context('missing', function () {
+      it('returns default value', function () {
         expect(requireOptional('missing', def)).to.equal(def)
       })
 
-      it('calls error function if passed in', function(){
+      it('calls error function if passed in', function () {
         var cb = sinon.spy()
         requireOptional('missing', null, cb)
         expect(cb).to.have.been.calledWithMatch({
